affb29c3330742db0b7120aa18f8a42c7c695832
maartenverheul <maarten.verheul@student.hu.nl>
2020-06-16 20:58:44 +0200

    Add base project

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# TimeOnce
-Keep track of time
+# timeonce
+
+> Keep track of time
+
+## Build Setup
+
+```bash
+# install dependencies
+$ yarn install
+
+# serve with hot reload at localhost:3000
+$ yarn dev
+
+# build for production and launch server
+$ yarn build
+$ yarn start
+
+# generate static project
+$ yarn generate
+```
+
+For detailed explanation on how things work, check out [Nuxt.js docs](https://nuxtjs.org).
---
+++ pages/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <section class="section">
+    <div class="columns is-fluid">
+      <div class="column">
+        <div class="card">
+          <div class="card-content">
+            <div class="content">
+              <div class="level">
+                <div class="level-left">
+                  <b-input v-model="description" :placeholder="placeholder" size="is-medium" style="width: 500px"></b-input>
+                </div>
+                <div class="level-right">
+                  <b-button type="is-danger" size="is-medium" @click="startTimer">00:00</b-button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "HomePage",
+  data: () => ({
+    description: "",
+    placeholder: "What are you about to do?"
+  }),
+  methods: {
+    startTimer(){
+      alert("hi");
+    }
+  }
+};
+</script>
